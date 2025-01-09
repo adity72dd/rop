e18674ef7573c59229a66e708b3ae99b7b83942a
adity72dd <mmunnabot7@gmail.com>
2025-01-09 10:51:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7579871789:AAGUEcdME6LIw__k3Z56k3NGM3anNh6nY1c")
+BOT_TOKEN=("7194542599:AAGn0a64pR-UTbrTb5udPrHJc25Lb2TKBac")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Mr_sid_001")
+OWNER_USERNAME=("Riyahacksyt")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("6479495033")
